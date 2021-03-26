14590267c532228b5bc5e7ac3a548b2929c7ad85
Matthew Diorio <matthewdiorio@icloud.com>
2021-03-26 19:09:17 -0400

    lab9 complete

---
+++ lab9/index.js
@@ -0,0 +1,58 @@
+import Express, {
+    response
+} from 'express'; //use express
+import fs from 'fs'; // use filesystem
+
+const App = Express();
+const port = 3010; // creating port 3010
+
+let fileContents = fs.readFileSync("database.json");
+
+let database = JSON.parse(fileContents);
+
+App.use("/", Express.static("public"));
+
+App.get("/ages/:number", (req, res) => { // getting employee by age
+    let result = {
+        "error": "not found"
+    }
+
+    database.forEach((value) => {
+        if (req.params.number == value.age) {
+            result = value;
+        }
+    });
+
+    res.json(result);
+})
+
+
+App.get("/employees/:name", (req, res) => { // getting employee by name
+    let result = {
+        "error": "not found"
+    }
+
+    database.forEach((value) => {
+        if (req.params.name == value.name) {
+            result = value;
+        }
+    });
+
+    res.json(result);
+})
+
+App.post("/employees/:name/:age/", (req, res) => {
+    let result = {
+        "name": req.params.name,
+        "age": parseInt(req.params.age)
+    };
+
+    database.push(result)
+
+    fs.writeFileSync("database.json", JSON.stringify(database, null, '\t'));
+
+    res.json(result);
+});
+App.listen(port, () => {
+    console.log("Server running") // if server is successfully running
+})
