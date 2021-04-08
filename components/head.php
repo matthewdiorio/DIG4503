01a4b0c08b03ad227ba32aeea3a6762bae8d166c
Yjang Wynter <21028927+YjangWynter@users.noreply.github.com>
2021-04-07 22:09:44 -0400

    added switch for styles & titles

--- components/head.php
+++ components/head.php
@@ -7,14 +7,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.1.1/css/all.css">
     <link rel="stylesheet" href="./styles/w3.css">
-    <link rel="stylesheet" href="./styles/styles.css">
+    <link rel="stylesheet" href="./styles/styles.css">    
+    <link rel="shortcut icon" href="./assets/logo/Favicon.png" type="image/x-icon">
     <?php
         $pageName = $_SERVER['PHP_SELF'];
         echo $pageName;
-        if($pageName == '/www/foodEx/application.php'){
-            echo ' <link rel="stylesheet" href="./styles/application.css">';
+        switch ($pageName){
+            case '/www/foodEx/application.php':
+                echo ' <link rel="stylesheet" href="./styles/application.css">';
+                echo '<title>New Application</title>';
+                break;
+            case '/www/foodEx/signup.php':
+                echo ' <link rel="stylesheet" href="./styles/signup.css>';
+                echo '<title>Sign Up</title>';
+                break;
+            case  '/www/foodEx/signin.php':
+                echo ' <link rel="stylesheet" href="./styles/signin.css>';
+                echo '<title>Sign In</title>';
+                break;
+            case  '/www/foodEx/dashboard.php':
+                echo ' <link rel="stylesheet" href="./styles/dashboard.css>';
+                echo '<title>Dashboard</title>';
+                break;    
+            case  '/www/foodEx/search.php':
+                echo ' <link rel="stylesheet" href="./styles/search.css>';
+                echo '<title>Search Banks</title>';
+                break;    
+            case  '/www/foodEx/insert.php':
+                echo ' <link rel="stylesheet" href="./styles/>';
+                echo '<title>Insert Title</title>';
+                break;    
+            default: 
+                echo '<title>FoodEx</title>';
+                break;
         }
     ?>
-    <link rel="shortcut icon" href="./assets/logo/Favicon.png" type="image/x-icon">
-    <title>FoodEx</title>
+    
+
 </head>
---
+++ signin.php
@@ -0,0 +1,51 @@
+<?php include 'components/head.php'; ?>
+<body>
+<?php include './components/topnav.php'; ?>
+
+    
+    <section class="menu">
+        <div class="w3-row w3-center">
+            <div class="w3-auto ">
+                <h2 class="w3-section">Are you a Patron or Bank?</h2>
+                <a class="link" href="../dashboard/patron/"><div class="w3-half   w3-mobile w3-card user-box">
+                    <img class="w3-image w3-margin" src="https://via.placeholder.com/500x250" alt="" srcset="">
+                    <h3>Patron </h3>
+
+                </div>
+            </a>
+            <a class="link" href="../dashboard/bank/">
+                <div class="w3-half w3-mobile w3-card user-box">
+                    <img class="w3-image w3-margin" src="https://via.placeholder.com/500x250" alt="" srcset="">
+                    <h3>Food Bank</h3>
+                </div>
+            </div>
+            </a>
+        </div>
+    </section>
+    <footer>
+        <div class="w3-margin-top w3-row w3-border  w3-bar">
+            <div class="w3-auto ">
+                <ul class="w3-ul w3-mobile ">
+                    <li class="w3-mobile w3-auto w3-bar-item">
+                        <a class="link" href="../">Home</a>
+                    </li>
+                    <li class="w3-mobile w3-auto w3-bar-item">
+                        <a class="link" href="../about/">About</a>
+                    </li>
+                    <li class="w3-mobile w3-auto w3-bar-item">
+                        <a class="link" href="../banks/">Banks</a>
+                    </li>
+                   
+                    <li class="w3-mobile w3-auto w3-bar-item">
+                        <a class="link" href="#">Terms & Conditions</a>
+                    </li>
+                    <li class="w3-mobile w3-auto w3-bar-item">
+                        <a class="link" href="#">Privacy Policy</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
