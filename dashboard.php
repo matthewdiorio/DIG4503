a310299e7d1caab97366399f2c98a8ddce91e212
YjangWynter <yjangwynter@gmail.com>
2021-04-08 23:13:23 -0400

    added dashboard & updated README

---
+++ dashboard.php
@@ -0,0 +1,11 @@
+<?php include 'components/Head.php'; ?>
+
+<?php include './components/MainNavigation.php'; ?>
+
+<?php 
+    echo "Must only be accessible after login/signin.
+    <br> Use session cookes.";
+?>
+
+
+<?php include "./components/Footer.php";?>
