876383003ee2709d44bdef1b41019f2a7563457a
YjangWynter <yjangwynter@gmail.com>
2021-04-10 17:51:29 -0400

    added qr code and dummy data for now

--- dashboard.php
+++ dashboard.php
@@ -5,9 +5,9 @@
 <?php 
     $userType = 'patron';
     if($userType == 'patron'){
-      
-        $foodEvents = [
-            {
+      /**
+       * 
+       *  {
                 key: 1,
                 photo: 'https://media-exp1.licdn.com/dms/image/C510BAQHlfm7KULdsBw/company-logo_200_200/0/1519889798320?e=2159024400&v=beta&t=TJS-PtLIAtwNCAA8Y799ggeWacedLqqhnWOXoWnFrrE',
                 name: 'Knights Helping Knights Pantry',
@@ -43,7 +43,30 @@
                 endTime:1800,
                 address:'1771 N Semoran Blvd, Orlando, FL 32807'
             }
+       */
+      //All just dummy data
+        $bankPictures = [
+           'https://media-exp1.licdn.com/dms/image/C510BAQHlfm7KULdsBw/company-logo_200_200/0/1519889798320?e=2159024400&v=beta&t=TJS-PtLIAtwNCAA8Y799ggeWacedLqqhnWOXoWnFrrE',
+           "https://pbs.twimg.com/profile_images/2924896269/3219fad95ba574ed71330b3f2503ca4a.jpeg",
+           "https://pbs.twimg.com/profile_images/1326276919231393794/UTJ1kGRS_400x400.jpg"
         ];
+        $bankNames = [
+            'Knights Helping Knights Pantry',
+            'Seeds of Paradise Food Pantry',
+            'Food Distribution Center - Semoran Food Pantry'
+        ];
+        $bankTimes= [
+            '10:00a.m. - 5:00p.m.',
+            '11:00a.m. - 3:00p.m.',
+            '8:30a.m. - 12:30p.m.'
+        ];
+        $bankDays = [
+            'Wednesday',
+            'Tuesday',
+            'Monday'
+        ];
+        $user
+
 
 
         ?> 
@@ -54,10 +77,10 @@
             <div class=" w3-section">
                 
                 <div class="w3-margin-left w3-center">
-                    <h2>Hello <?php 
+                    <h2><strong>Welcome, <?php 
                     if(!isset($full_name)){
                         echo 'Default User';
-                    }?></h2>
+                    }?></strong></h2>
                 </div>
                 <div class="w3-auto applications w3-auto  w3-light-grey w3-round w3-border w3-center w3-section"> 
                 
@@ -68,29 +91,38 @@
         </div>
         <div class="w3-half w3-mobile">
             <div class=" w3-section">
-                <div class="w3-auto sidebar  w3-center w3-section">
+                <div class="w3-auto w3-margin-right">
+                <h2>Events This Week</h2>
                 <?php 
-                    if(count($foodEvents) > 0){
-                        $len_events = count($foodEvents);
-                        for ($i = 0; i < $len_events; i++){
+                    if(count($bankPictures) > 0){
+                        $len_events = count($bankPictures);
+                        
+                        for ($i = 0; $i < $len_events; $i++){
                             ?>
-                            <div class="w3-card">
-                                <div class="w3-bar">
-                                    <img class="w3-image w3-mobile" src="<?php echo $foodEvents[$i].photo; ?>"/>
+                                <div class="w3-panel w3-white w3-card w3-bar w3-auto">
+                                    <img style="max-width:5vw;" class="w3-bar-item w3-circle w3-mobile" src="<?php echo $bankPictures[$i]; ?>"/>
+                                    <div class="w3-bar-item w3-auto">
+                                        <strong class=" w3-section"><?php echo $bankNames[$i]; ?></strong>
+                                        <p class=" w3-section" ><?php echo $bankDays[$i].' '.$bankTimes[$i]; ?></p>
+                                    </div>
+                                    
                                 </div>
-                            </div>
                             <?php
                         }
                     }
                 ?>
                 </div>
             </div>
+            <div class="w3-auto w3-margin w3-padding ">
+            <h3><strong>Scan Your QR</strong></h3>
+            <img style="width:10vw;"class="w3-mobile w3-image w3-auto w3-light-grey w3-round w3-border  " src="./assets/qr_default.png" alt="default qr code">
+           </div>
         </div>
 
     </div>
     <div class="w3-row w3-auto profile profile-user w3-mobile w3-center">
         <div class="w3-half w3-auto w3-mobile w3-margin w3-padding w3-light-grey w3-round w3-border  w3-center">Edit Profile</div>
-        <div class="w3-half w3-auto w3-mobile w3-margin w3-padding w3-light-grey w3-round w3-border  w3-center">Scan QR</div>
+       
 
     </div>
 
