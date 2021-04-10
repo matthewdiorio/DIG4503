6da9c684af3bb8593e34cf7cfdac069b3dc194ed
YjangWynter <yjangwynter@gmail.com>
2021-04-10 18:52:49 -0400

    added style to dashboard

--- dashboard.php
+++ dashboard.php
@@ -65,7 +65,6 @@
             'Tuesday',
             'Monday'
         ];
-        $user
 
 
 
@@ -82,9 +81,30 @@
                         echo 'Default User';
                     }?></strong></h2>
                 </div>
-                <div class="w3-auto applications w3-auto  w3-light-grey w3-round w3-border w3-center w3-section"> 
+                <div class="w3-auto w3-card applications w3-round w3-border "> 
                 
-                Applications Status
+                    <div class="w3-right app-div w3-padding-large w3-left w3-auto">
+
+                    <h4>Food Distribution Center <br> Semoran Food Pantry</h4>
+                    <p>1771 N Semoran Blvd, <br/>Orlando, FL 32807</p>
+                    <strong>In Review</strong>
+                    </div>
+                </div>
+
+            </div>
+            <div class=" w3-section">
+                
+                <div class="w3-margin-left w3-center">
+                    <h2><strong> Upcoming Appointments</strong></h2>
+                </div>
+                <div class="w3-auto w3-card applications appointment w3-round w3-border "> 
+                
+                    <div class="w3-right app-div w3-padding-large w3-left w3-auto">
+
+                    <h4>Knights Helping <br/>Knights Pantry</h4>
+                    <p>4133 Pyxis Ln,<br/> Orlando, FL 32816</p>
+                    <strong>Wednesday, 10:30a.m.</strong>
+                    </div>
                 </div>
 
             </div>
