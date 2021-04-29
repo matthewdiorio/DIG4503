ae37d5475bc8ff3f32eec37612870ae6c928793b
matthewdiorio <37198911+matthewdiorio@users.noreply.github.com>
2021-04-28 23:41:04 -0400

    Portfolio 2

--- lab9/index.js
+++ lab9/index.js
@@ -1,27 +1,35 @@
 import Express, {
     response
 } from 'express'; //use express
-import fs from 'fs'; // use filesystem
 
+import fs from 'fs'; // use filesystem
+import chalk from 'chalk'; // importing chalk
+import validator from "validator"; //importing validator
 const App = Express();
 const port = 3010; // creating port 3010
 
 let fileContents = fs.readFileSync("database.json");
 
-let database = JSON.parse(fileContents);
+let database = JSON.parse(fileContents); // creating databavse
 
-App.use("/", Express.static("public"));
+App.use("/", Express.static("public")); //root
 
 App.get("/ages/:number", (req, res) => { // getting employee by age
     let result = {
-        "error": "not found"
+        "error": "not found" // changing result to not found
+    }
+    if (validator.isNumeric(req.params.number)) { // checks if it is a number
+        database.forEach((value) => { // looping through database
+            if (req.params.number == value.age) { // if input matches an age
+                result = value; // setting result 
+            }
+        });
+    } else{ // not a number
+        result = {
+            "error": "only enter a number!"
+        }
     }
 
-    database.forEach((value) => {
-        if (req.params.number == value.age) {
-            result = value;
-        }
-    });
 
     res.json(result);
 })
@@ -29,19 +37,19 @@ App.get("/ages/:number", (req, res) => { // getting employee by age
 
 App.get("/employees/:name", (req, res) => { // getting employee by name
     let result = {
-        "error": "not found"
+        "error": "not found" //default value
     }
 
-    database.forEach((value) => {
-        if (req.params.name == value.name) {
-            result = value;
+    database.forEach((value) => { //looping through database
+        if (req.params.name == value.name) { // if input matches a name
+            result = value; //dsiplaying result the value
         }
     });
 
     res.json(result);
 })
 
-App.post("/employees/:name/:age/", (req, res) => {
+App.post("/employees/:name/:age/", (req, res) => { //posting new employee
     let result = {
         "name": req.params.name,
         "age": parseInt(req.params.age)
@@ -54,5 +62,5 @@ App.post("/employees/:name/:age/", (req, res) => {
     res.json(result);
 });
 App.listen(port, () => {
-    console.log("Server running") // if server is successfully running
+    console.log(chalk.blue("Server running")); // if server is successfully running changed to blue to seperate from green nodemon
 })
